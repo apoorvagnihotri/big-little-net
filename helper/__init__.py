f14dfa4b1f87d363cf47ba911b536f4aeab00e13
Apoorv Agnihotri <apoorvagni@hotmail.com>
2019-03-12 19:12:42 +0530

    Runner code (#1) Merge
    * files for importing models
    
    * added modified options file
    
    * added function to find available models
    
    * removed resnet.py, using models from torchvision
    
    * added runner code from pytorch examples
    
    * updated README with updates and new plans, added few ignores in.gitignore
    
    * modified .gitignore

--- helper/__init__.py
+++ helper/__init__.py
@@ -4,3 +4,4 @@ This module gathers all utils
 
 from .data_manager import downloader
 from .options import read_args
+from .utils import *
--- helper/options.py
+++ helper/options.py
@@ -1,12 +1,54 @@
-import os
 import argparse
 
-home_dir = '../'
-
-def read_args():
-    # Parse arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--data_loc", default=os.path.join(home_dir,'data'),
-                        help="Location from where to load the data")
-    args = parser.parse_args()
-    return args
+def read_args(model_names):
+    parser = argparse.ArgumentParser(description='ImageNet Training using Big-Little Network')
+    parser.add_argument('data', metavar='DIR', help='path to dataset')
+    parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
+                        choices=model_names,
+                        help='model architecture: ' +
+                            ' | '.join(model_names) +
+                            ' (default: resnet18)')
+    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                        help='number of data loading workers (default: 4)')
+    parser.add_argument('--epochs', default=110, type=int, metavar='N',
+                        help='number of total epochs to run')
+    parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                        help='manual epoch number (useful on restarts)')
+    parser.add_argument('-b', '--batch-size', default=256, type=int,
+                        metavar='N',
+                        help='mini-batch size (default: 256), this is the total '
+                             'batch size of all GPUs on the current node when '
+                             'using Data Parallel or Distributed Data Parallel')
+    parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                        metavar='LR', help='initial learning rate', dest='lr')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                        metavar='W', help='weight decay (default: 1e-4)',
+                        dest='weight_decay')
+    parser.add_argument('-p', '--print-freq', default=10, type=int,
+                        metavar='N', help='print frequency (default: 10)')
+    parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                        help='path to latest checkpoint (default: none)')
+    parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                        help='evaluate model on validation set')
+    parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                        help='use pre-trained model')
+    parser.add_argument('--world-size', default=-1, type=int,
+                        help='number of nodes for distributed training')
+    parser.add_argument('--rank', default=-1, type=int,
+                        help='node rank for distributed training')
+    parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
+                        help='url used to set up distributed training')
+    parser.add_argument('--dist-backend', default='nccl', type=str,
+                        help='distributed backend')
+    parser.add_argument('--seed', default=None, type=int,
+                        help='seed for initializing training. ')
+    parser.add_argument('--gpu', default=None, type=int,
+                        help='GPU id to use.')
+    parser.add_argument('--multiprocessing-distributed', action='store_true',
+                        help='Use multi-processing distributed training to launch '
+                             'N processes per node, which has N GPUs. This is the '
+                             'fastest way to use PyTorch for either single node or '
+                             'multi node data parallel training')
+    return parser.parse_args()
